3a42b5e4f16c525b75189589062a69c5f490e92e
Sakib Khan Inan <sakib.khaninan@gmail.com>
2019-09-25 13:19:50 +0600

    update lecture 3

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
-# Python script for basic Insertion Sort and Binary Insertion Sort.
+# Python script to perform Insertion Sort and Binary Insertion Sort on a given 1D array or list.
+# Worst Case Complexity O(n^2)
 
 
-def binary_search(array, key, start, end):
-    """ Using binary search method to find the accurate place for 'key'
+def binary_search(array, key, starting_index, ending_index):
+    """ Using binary search method to find the accurate(inplace) place for 'key'
     to reduce the complexity for comparisons of insertion sort"""
-    if start == end:
-        if array[start] > key:
-            return start
+    if starting_index == ending_index:
+        if array[starting_index] > key:
+            return starting_index
         else:
-            return start + 1
-    if start > end:
-        return start
-    mid = (start + end) // 2
+            return starting_index + 1
+    if starting_index > ending_index:
+        return starting_index
+
+    # Binary Search process starts from here.
+    mid = (starting_index + ending_index) // 2
     if array[mid] < key:
-        return binary_search(array, key, mid + 1, end)
+        return binary_search(array, key, mid + 1, ending_index)
     elif array[mid] > key:
-        return binary_search(array, key, start, mid)
+        return binary_search(array, key, starting_index, mid)
     elif array[mid] == key:
         return mid
 
@@ -30,24 +33,27 @@ def binary_insertion_sort(array):  # Function for Binary Insertion Sort
             array[i] = array[i - 1]
             i = i - 1
         array[j] = key
-    return array
 
 
 def insertion_sort(array):  # Function for Insertion Sort
-    for i in range(1, len(array)):
-        key = array[i]
+    for i in range(1, len(array)):  # (n - 1) passes for n elements in a list.
+        key = array[i]  # Taking every element in the list as a key in every iteration.
         j = i - 1
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
         array[j + 1] = key
-    return array
 
 
 def main():
     array: list = [8, 2, 4, 9, 3, 6]  # Test Array / List
-    print(insertion_sort(array))  # Returns a sorted list in ascending order.
-    print(binary_insertion_sort(array))  # Returns a sorted list in ascending order.
+    insertion_sort(array)  # Returns a sorted list in ascending order.
+    print("Insertion Sort\n" + "Given Array/list: ", array)
+    print("After Sorting: ", array)
+    array1: list = [5, 2, 10, 9, 11, 6]  # Test Array / List
+    binary_insertion_sort(array1)  # Returns a sorted list in ascending order.
+    print("Binary Insertion Sort\n"+"Given Array/list: ", array1)
+    print("After Sorting: ", array1)
 
 
 if __name__ == "__main__":
---
+++ merge_sort.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# Python script to perform Merge Sort on a given array/ list / matrix(1D)
+# Worst Case Complexity O(nlog(n))
+
+import math
+
+
+def merge(array_list, start_index, mid, end_index):
+    length_left = (mid - start_index) + 1  # Length of a left sublist.
+    length_right = end_index - mid  # Length of right sublist.
+
+    left_sublist = [None]*length_left  # Create a left sublist.
+    right_sublist = [None]*length_right  # Create a right sublist.
+
+    # Assign elements to left and right sublist.
+    for i in range(0, length_left):
+        left_sublist[i] = array_list[start_index + i]
+    for j in range(0, length_right):
+        right_sublist[j] = array_list[mid + j + 1]
+
+    # Append "infinity" as the last element of two sublist.
+    left_sublist.append(math.inf)
+    right_sublist.append(math.inf)
+    
+    i = 0
+    j = 0
+
+    # Finally sort elements by comparing elements of left and right sublist.
+    for k in range(start_index, end_index + 1):
+        if left_sublist[i] <= right_sublist[j]:
+            array_list[k] = left_sublist[i]
+            i = i + 1
+        else:
+            array_list[k] = right_sublist[j]
+            j = j + 1
+
+
+def merge_sort(array_list, start_index, end_index):
+    if start_index < end_index:
+        mid = (start_index + end_index)//2
+        merge_sort(array_list, start_index, mid)  # Create left sublist (n/2).
+        merge_sort(array_list, mid + 1, end_index)  # Create right sublist (n/2).
+        merge(array_list, start_index, mid, end_index)  # Merge created sub-lists in recursive manner.
+
+
+def main():
+    array_list: list = [8, 2, 4, 9, 3, 6]  # Test Array / List
+    print("Given Array/List: ", array_list)
+    merge_sort(array_list, 0, len(array_list) - 1)
+    print("After Sorting: ", array_list)
+
+
+if __name__ == "__main__":
+    main()
